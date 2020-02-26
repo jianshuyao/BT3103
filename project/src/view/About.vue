8e56fcab9cddd78cb7d80dae700bcd32b972615b
yidi <yidi.hu@epfl.ch>
2020-02-26 19:10:52 +0800

    About and Contact pages

--- project/src/view/About.vue
+++ project/src/view/About.vue
@@ -1,14 +1,164 @@
 <template>
-  <div>
-    <h1>About Us</h1>
-    <p>Here is some information. This is the about page</p>
+<div>
+  <!-- Team -->
+      <div class="basic-2">
+          <div class="container">
+
+              <div class="row">
+                  <div class="col-lg-12">
+                      <h2>Who we are</h2>
+                      <br>
+                  </div> <!-- end of col -->
+              </div> <!-- end of row -->
+
+              <div class="row">
+                  <div class="col-lg-12">
+
+                      <!-- Team Member -->
+                      <div class="team-member">
+                          <div class="image-wrapper">
+                              <img class="img-fluid" src="./images/DH.jpg" alt="alternative" >
+                          </div> <!-- end of image-wrapper -->
+
+                          <p class="p-large">Dong Han</p>
+                          <p class="job-title">General Manager</p>
+                      </div> <!-- end of team-member -->
+                      <!-- end of team member -->
+
+                      <div class="team-member">
+                          <div class="image-wrapper">
+                              <img class="img-fluid" src="./images/YD.jpg" alt="alternative" >
+                          </div> <!-- end of image-wrapper -->
+                          
+                          <p class="p-large">Hu Yidi</p>
+                          <p class="job-title">Business Analyst</p>
+                      </div> <!-- end of team-member -->
+                      <!-- end of team member -->
+
+                      <div class="team-member">
+                          <div class="image-wrapper">
+                              <img class="img-fluid" src="./images/SY.jpg" alt="alternative" >
+                          </div> <!-- end of image-wrapper -->
+                          
+                          <p class="p-large">Jian Shuyao</p>
+                          <p class="job-title">Web Developer</p>
+                      </div> <!-- end of team-member -->
+                      <!-- end of team member -->
+
+                      <div class="team-member">
+                          <div class="image-wrapper">
+                              <img class="img-fluid" src="./images/BX.jpg" alt="alternative" >
+                          </div> <!-- end of image-wrapper -->
+                          
+                          <p class="p-large">Yang Bixuan</p>
+                          <p class="job-title">Market Researcher</p>
+                      </div> <!-- end of team-member -->
+                      <!-- end of team member -->
+
+                  </div> <!-- end of col -->
+              </div> <!-- end of row -->
+
+          </div> <!-- end of container -->
+      </div> <!-- end of basic-2 -->
+    
+
+    
+    <!-- Details 1 -->
+	
+  
+  <div id="details" class="accordion">
+		<div class="area-1"></div><!-- end of area-1 on same line and no space between comments to eliminate margin white space -->
+    
+    <div class="area-2">
+
+            <!-- Accordion -->
+            <div class="accordion-container" id="accordionOne">
+                <h2>What we do</h2>
+                <div class="item">
+                    <div id="headingOne">
+                        <span data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" role="button">
+                            <span class="circle-numbering">1</span><span class="accordion-title">How Can Aria Help Your Business</span>
+                        </span>
+                    </div>
+                </div> <!-- end of item -->
+            
+                <div class="item">
+                    <div id="headingTwo">
+                        <span class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" role="button">
+                            <span class="circle-numbering">2</span><span class="accordion-title">Great Strategic Business Planning</span>
+                        </span>
+                    </div>
+                    
+                </div> <!-- end of item -->
+            
+                <div class="item">
+                    <div id="headingThree">
+                        <span class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" role="button">
+                            <span class="circle-numbering">3</span><span class="accordion-title">Online Marketing Campaigns</span>
+                        </span>
+                    </div>
+                </div> <!-- end of item -->
+
+            </div> <!-- end of accordion-container -->
+            <!-- end of accordion -->
+
+		</div> <!-- end of area-2 -->
+  </div> <!-- end of accordion -->
+  <!-- end of details 1 -->
+
+
+    <!-- Details 2 -->
+	<div class="tabs">
+      <div class="area-1">
+            <div class="tabs-container" id="accordionOne">
+                <h2>Why choose us</h2>
+                <br>
+                <div class="item">
+                    <div id="headingOne">
+                        <span data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" role="button">
+                            <span class="circle-numbering">1</span>
+                            <span class="accordion-title">How Can Aria Help Your Business</span>
+                        </span>
+                    </div>
+                </div> <!-- end of item -->
+            
+                <div class="item">
+                    <div id="headingTwo">
+                        <span class="collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" role="button">
+                            <span class="circle-numbering">2</span><span class="accordion-title">Great Strategic Business Planning</span>
+                        </span>
+                    </div>
+                    
+                </div> <!-- end of item -->
+            
+                <div class="item">
+                    <div id="headingThree">
+                        <span class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" role="button">
+                            <span class="circle-numbering">3</span><span class="accordion-title">Online Marketing Campaigns</span>
+                        </span>
+                    </div>
+                </div> <!-- end of item -->
+
+            </div>
+        </div><!-- end of area-1 on same line and no space between comments to eliminate margin white space -->
+        
+        <div class="area-2"></div> <!-- end of area-2 -->
+    </div> <!-- end of tabs -->
+    <!-- end of details 2 -->
   </div>
+  <!-- end of team -->
+
+  
 </template>
+
+
 
 <script>
 export default {
   name: 'About' //this is the name of the component
 }
 </script>
-<style>
+
+<style scoped>
+@import './css/styles.css'; 
 </style>
--- project/src/view/Contact.vue
+++ project/src/view/Contact.vue
@@ -1,23 +1,86 @@
 <template>
-  <div>
-    <h1>Contact Us</h1>
-  <p>
-    <ul>
-      <li>Name: {{name}}</li>
-      <li>Email: {{email}}</li>
-    </ul>
-  </p>
-  </div>
+<div id="contact" class="form-2">
+    <div class="container">
+        <div class="row">
+            
+            <!-- Map -->
+            <div class="col-lg-6">
+                <div class="text-container">
+                    <h2>Talk to Us!</h2>
+                    <ul class="list-unstyled li-space-lg">
+                        <li class="address"><i class="fas fa-map-marker-alt"></i>21 Heng Mui Keng Terrace, Icube Building, Singapore 119613</li>
+                        <li><i class="fas fa-envelope"></i><a href="mailto:office@withdrawal.com">TEAM@withdrawal.com</a></li>
+                    </ul>
+                    <iframe width="500" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=i3%20Auditorium&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div> <!-- end of text-container -->
+            </div> <!-- end of col -->
+
+            <!-- Contact Form -->
+            <div class="col-lg-6">
+                <form id="contactForm" data-toggle="validator" data-focus="false">
+                    <div class="form-group">
+                        <input type="text" class="form-control-input" id="cname" required placeholder='Name'>
+                        <label class="label-control" for="cname"></label>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control-input" id="cemail" required placeholder='Email'>
+                        <label class="label-control" for="cemail"></label>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control-textarea" id="cmessage" required placeholder='Message'> </textarea>
+                        <label class="label-control" for="cmessage"></label>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                    <div class="form-group checkbox">
+                        <input type="checkbox" id="cterms" value="Agreed-to-Terms" required>I agree with Aria's stated <a href="privacy-policy.html">Privacy Policy</a> and <a href="terms-conditions.html">Terms Conditions</a> 
+                        <div class="help-block with-errors"></div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="form-control-submit-button">SUBMIT MESSAGE</button>
+                    </div>
+                    <div class="form-message">
+                        <div id="cmsgSubmit" class="h3 text-center hidden"></div>
+                    </div>
+                </form>
+                <!-- end of contact form -->
+
+            </div> <!-- end of col -->
+            
+        </div> <!-- end of row -->
+    </div> <!-- end of container -->
+</div> <!-- end of form-2 -->
+        <!-- end of contact -->
+
 </template>
 
 <script>
-export default {
-  name: 'Contact',
-  data(){
-    return {
-      name: 'JSY',
-      email: 'jianshuyao@u.nus.edu'
+  export default {
+    name: 'Contact',
+    data(){
+      return {
+        name: 'JSY',
+        email: 'jianshuyao@u.nus.edu'
+      }
     }
   }
-}
 </script>
+
+<style scoped>
+/* @import './css/bootstrap.css';
+@import './css/styles.css';
+@import './css/fontawesome-all.css';
+@import './css/magnific-popup.css';
+@import './css/swiper.css'; */
+
+.form-group {
+  color: black;
+}
+
+.form-2 .text-container {
+  margin-bottom: 3rem;
+  color: black;
+}
+
+</style>
