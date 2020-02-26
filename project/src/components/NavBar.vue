f4fe664c4306d6f56a9d31883b4bc26b539a673d
jianshuyao <jianshuyao@u.nus.edu>
2020-02-26 23:38:08 +0800

    new changes

--- project/src/components/NavBar.vue
+++ project/src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-sm navbar-light" style="background-color: #ededed;">
       <div class="container-fluid">
         <div class="navbar-header">
-          <img src="../assets/nus.png" width="100" height="40" style="margin:0px 30px 0px 0px">
+          <img src="../assets/nus.png" width="100" height="42" style="margin:0px 30px 0px 0px">
         </div>
         <ul class="nav navbar-nav">
           <li>
