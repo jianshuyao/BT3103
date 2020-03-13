4d3dd65bf9a1fe905d02777518c7c59641138e84
invisiblehan <46435224+invisiblehan@users.noreply.github.com>
2020-03-13 21:47:45 +0800

    retrieve from firebase

--- project/src/view/University.vue
+++ project/src/view/University.vue
@@ -1,112 +1,80 @@
 <template>
-<div v-cloak>
+ <div v-cloak>
   <router-view></router-view>
- <div class="container">
-  <div class="col-15">
-    <div class="md-form">
-        <input type="text" class="form-control" placeholder="Search Opportunities" v-model="filter">
-      </div>
-  </div>
-  <br>
-  
-    <el-col v-for="university in getUniversities" v-bind:key="university.name" :xs="24" :sm="12" :md="8">
-      <div class="box"> 
-        <div> {{ university.name }} </div>
-        <div class="box__subtitle"> {{ university.country }} </div>
-        <div class="box__subtitle"> {{ university.length }} Months </div>
-        <router-link v-bind:to = "'/'+ university.link" exact>Go to University Page</router-link>
-      </div>
-    </el-col>
+  <div class="container">
+    <div class="col-15">
+      <div class="md-form">
+          <input type="text" class="form-control" placeholder="Search Opportunities" v-model="filter">
+        </div>
+    </div>
+    <br>
+    <ul>
+        <li v-for="university in getUniversities" v-bind:key="university.id">
+          <div class="box"> 
+            <div> {{ university.university }} </div>
+            <div class="box__subtitle"> {{ university.location }} </div>
+            <div class="box__subtitle"> {{ university.semesterLength}} Months </div>
+            <router-link v-bind:to = "'/'+ university.link" exact>Go to University Page</router-link>
+          </div>
+        </li>
 
-    <el-col v-if="getUniversities.length === 0" :xs="24" :sm="24" :md="24">
-      <div class="box box__empty"> No Match Found</div>
-    </el-col>
-
+       <div v-if="getUniversities.length === 0" >
+        <div class="box box__empty"> No Match Found</div>
+       </div>
+    </ul>
     
- </div> <!-- container -->
-  
-</div> <!-- #app -->
-
+  </div>
+</div>
 </template>
 
 
 
 <script>
-export default{
-  name: 'University', 
+import database from '../firebase.js';
+export default {
+  data(){
+    return{
+        uniList: [],
+        filter: '',
+        }
+  },
 
-  data() {
-    return { 
-      filter: '',
-      options: [
-        { label: 'Default', value: 'none' },
-        { label: 'By Program Length', value: 'length' },
-      ],
-      universities: [
-        { name: 'University of California, Santa Barbara', country: 'United States', length: 3, link:'University/UCSB'},
-        { name: 'Peking University', country: 'China', length: 6, link:'University/PKU'},
-        { name: 'Tokyo University', country: 'Japan', length: 5, link:'/tku.vue'},
-        { name: 'Fudan University', country: 'China', length: 6, link:'/fdu.vue'},
-        { name: 'Oxford University', country: 'United Kingdom', length: 4, link:'oxf.vue'},
-        { name: 'Nanyang Technological University', country: 'Singapore', length: 4, link:'/ntu.vue'}
-      ],
-    };
+  methods:{
+    fetchUniversities:function(){
+      let university={}
+      //Get all the items from DB
+      database.collection('programs').get().then((querySnapShot)=>{
+        //Loop through each item
+        querySnapShot.forEach(doc=>{
+            //console.log(doc.id+"==>"+doc.data())
+            university=doc.data()
+            university.show=false
+            university.id=doc.id
+            this.uniList.push(university)
+        })
+      })
+    }, 
+  },
+  //Lifecycle hook
+  created(){
+      this.fetchUniversities()
   },
 
   computed: {
     getUniversities() {
-      
-      var universities = this.universities.filter((university) => {
-          return university.name.toLowerCase().includes(this.filter.toLowerCase());
+      var universities = this.uniList.filter((university) => {
+          return university.university.toLowerCase().includes(this.filter.toLowerCase());
         });
-      
-      if (this.sort == 'views') {
-        return universities.sort(function(a, b) {
-          return b.length - a.length
-        });
-      } else {
-        return universities;
-      }
+      return universities
       
     }
   }
 }
 
+
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Open+Sans');
-
-$material-shadow: 0 1px 3px 0 rgba(0,0,0,0.15);
-
-[v-cloak] {
-  display: none;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #F3F5F7;
-  font-family: 'Open Sans', sans-serif;
-}
-
-.search-wrapper {
-  margin: 10px 0;
-}
-
-.col-space {
-  content: '&nbsp;';
-  @media screen and (max-width: 767px) { display: none; }
-}
-
-.container {
-  max-width: 980px;
-  margin: 20px auto;
-  @media screen and (max-width: 1050px) { 
-    width: 95%; 
-  }
-}
-
 .box {
   display: flex;
   flex-direction: column;
@@ -126,7 +94,4 @@ body {
   &:hover { cursor: pointer; }
 }
 
-.el-select {
-  width: 100%;
-}
 </style>
